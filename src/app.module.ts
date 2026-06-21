70a23946c1444b8160ce561b3d9355e61e27c5ae
Anuj <anujtest23@gmail.com>
2026-06-21 21:24:33 +0545

    chore: pre-missing endpoints checkpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { BookingsModule } from './modules/bookings/bookings.module';
 import { PaymentsModule } from './modules/payments/payments.module';
 import { ReviewsModule } from './modules/reviews/reviews.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
+import { AdminModule } from './modules/admin/admin.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
@@ -29,6 +30,7 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
     PaymentsModule,
     ReviewsModule,
     NotificationsModule,
+    AdminModule,
   ],
   providers: [
     { provide: APP_FILTER, useClass: AllExceptionsFilter },
